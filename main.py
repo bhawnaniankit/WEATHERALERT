c9c0acfa0eac3fd6584acfd97f1a0a33752bc64f
bhawnaniankit <bhawnaniankit@gmail.com>
2023-11-29 23:38:50 +0530

    tested for rain

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ canvas.config(highlightthickness=0,width=340,height=600)
 
 weather=rain.weathe_city(CITY)
 id=weather[1]["weather"][0]["id"]
-id=302
+# id=801
 background=None
 text_color="white"
 
@@ -38,19 +38,29 @@ elif 800<id<805:
     
 
 canvas.create_image(170,300,image=background)
-city=canvas.create_text(170,75,text=CITY,fill=text_color,font=('Helvetica',12,"normal" ))
-temperature=canvas.create_text(170,110,text=f"{temp(weather)}{degree_sign}",fill=text_color,font=('Helvetica',35,"bold" ))
-
+city=canvas.create_text(170,110,text=f"{CITY}  ",fill=text_color,font=('Helvetica',12,"normal" ))
+temperature=canvas.create_text(170,150,text=f"{temp(weather)}{degree_sign}",fill=text_color,font=('Helvetica',45,"bold" ))
 canvas.create_line(0, 340, 340, 340, width=1,fill=text_color)
+
+canvas.create_text(165,190,text=f"{round(weather[1]["main"]["temp_max"]-273,1)}{degree_sign}/{round(weather[1]["main"]["temp_min"]-273,1)}{degree_sign}",fill=text_color,font=('Helvetica',12,"bold" ))
+
+sunrise=weather[1]["sys"]["sunrise"]
+sunrise=datetime.datetime.fromtimestamp(sunrise)
+sunset=weather[1]["sys"]["sunset"]
+sunset=datetime.datetime.fromtimestamp(sunset)
+sr_hr=sunrise.hour if sunrise.hour>9 else f"0{sunrise.hour}"
+sr_min=sunrise.minute if sunrise.minute>9 else f"0{sunrise.minute}"
+canvas.create_text(60,365,text=f"Sunrise: {sr_hr}:{sr_min}",fill=text_color,font=('Helvetica',12,"bold" ))
+ss_hr=sunset.hour if sunset.hour>9 else f"0{sunset.hour}"
+ss_min=sunset.minute if sunset.minute>9 else f"0{sunset.minute}"
+canvas.create_text(60,390,text=f"Sunset: {ss_hr}:{ss_min}",fill=text_color,font=('Helvetica',12,"bold" ))
+
+
+canvas.create_text()
 canvas.pack()
-
-
-canvas.pack(side='top', fill='both', expand='yes')
-
-
 
 if weather[0]:
     mail.alert_email("bhawnaniankit@gmail.com")
-    messagebox.showwarning("Weather Alert","")
+    messagebox.showinfo("Weather Alert","WEATHER ALERT\nSuggested to carry an umbrella")
     
 window.mainloop()
